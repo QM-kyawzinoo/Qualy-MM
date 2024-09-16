adf0eadb4a119ec0a183920f601844505762ae4e
QM-kyawzinoo <kyawzinoo@qualymm@gmail.com>
2024-09-16 09:35:19 +0630

    Second MV design

---
+++ assets/js/common.js
@@ -0,0 +1,23 @@
+// Your JavaScript code goes here
+document.addEventListener("DOMContentLoaded", () => {});
+
+// Your jQuery code goes here
+$(function () {
+  // Your code here
+  //   $("button").on("click", () => {
+  //     console.log("Hello");
+  //   });
+});
+
+$(document).ready(function () {
+  $(".menu-background").on("click", function () {
+      $(".header-menu").toggleClass("open-menu");
+      if($(".header-menu").hasClass("open-menu")){
+        $(".menu-background").html("<img src='/assets/img/aa.png' class='sp menu-image1'>");}
+        else{
+          $(".menu-background").html("<img src='/assets/img/img_menu.png' class='sp menu-image'>");
+        }
+      }
+  );
+  
+ })
