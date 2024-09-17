6c81ff95897a5dbac0454de16be76d9e9ccff4f0
QM-kyawzinoo <kyawzinoo@qualymm@gmail.com>
2024-09-17 08:40:57 +0630

    CSS Changed

--- assets/js/common.js
+++ assets/js/common.js
@@ -13,9 +13,9 @@ $(document).ready(function () {
   $(".menu-background").on("click", function () {
       $(".header-menu").toggleClass("open-menu");
       if($(".header-menu").hasClass("open-menu")){
-        $(".menu-background").html("<img src='/assets/img/aa.png' class='sp menu-image1'>");}
+        $(".menu-background").html("<img src='../assets/img/aa.png' class='sp menu-image1'>");}
         else{
-          $(".menu-background").html("<img src='/assets/img/img_mu.png' class='sp menu-image'>");
+          $(".menu-background").html("<img src='../assets/img/img_mu.png' class='sp menu-image'>");
         }
       }
   );
