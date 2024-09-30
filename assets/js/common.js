207beec07c79e530c5dab8909304079229d46149
QM-kyawzinoo <kyawzinoo@qualymm@gmail.com>
2024-09-30 16:31:18 +0630

    Hokaido

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,23 +1,57 @@
-// Your JavaScript code goes here
-document.addEventListener("DOMContentLoaded", () => {});
-
-// Your jQuery code goes here
-$(function () {
-  // Your code here
-  //   $("button").on("click", () => {
-  //     console.log("Hello");
-  //   });
+$(document).ready(function () {
+  $(".nav-btn").on("click", function () {
+    $(".nav-btn").toggleClass("close-btn");
+    $(".nav-list").toggleClass("open-menu");
+    $("body").toggleClass("scrollclose");
 });
 
-$(document).ready(function () {
-  $(".menu-background").on("click", function () {
-      $(".header-menu").toggleClass("open-menu");
-      if($(".header-menu").hasClass("open-menu")){
-        $(".menu-background").html("<img src='../assets/img/aa.png' class='sp menu-image1'>");}
-        else{
-          $(".menu-background").html("<img src='../assets/img/img_mu.png' class='sp menu-image'>");
-        }
-      }
-  );
+$(".nav-list a").on("click", function () {
+    // Close the menu and navigate to the corresponding page
+    $(".nav-btn").removeClass("close-btn");
+    $(".nav-list").removeClass("open-menu");
+    $("body").removeClass("scrollclose");
+    
+    // Navigate to the link's href
+    window.location.href = $(this).attr("href");
+});
   
- })
+  $(".accordion-content").css("display","none");
+  // $(".accordion-title:first-of-type").addClass("open");
+  $(".accordion-title").click(function(){
+   $(".open").not(this).removeClass("open").next().slideUp(300);
+   $(this).toggleClass("open").next().slideToggle(500);
+  });
+
+$(window).scroll(function () {
+  const faqSec = $(".sec-accordion");
+  const scrollPosition = $(window).scrollTop();
+  const faqSecOffset = faqSec.offset().top;
+
+  if (scrollPosition <= 500) {
+    $(".pagetop").addClass("d-none");
+  } else {
+    $(".pagetop").removeClass("d-none");
+  }
+
+  if ($(window).width() <= 768) {
+    if (scrollPosition >= faqSecOffset + 200) {
+      $(".scrolltop").addClass("pos");
+    } else {
+      $(".scrolltop").removeClass("pos");
+    }
+  } else {
+    if (scrollPosition >= faqSecOffset) {
+      $(".scrolltop").addClass("pos");
+    } else {
+      $(".scrolltop").removeClass("pos");
+    }
+  }
+});
+
+//To Top Click
+$(".pagetop").click(() => {
+  window.scrollTo(0, 0);
+});
+
+});
+
